a05a558df52b195b7832f833bf6cc26d19fa527e
HoCG <rhwkd5012@naver.com>
2024-06-11 00:34:17 +0900

    feat: 1-1 ~ 1-3

---
+++ chapter1/main.mjs
@@ -0,0 +1,9 @@
+import { statement } from "./1-1.js";
+
+import invoices from "./invoices.json" assert { type: "json" };
+import plays from "./plays.json" assert { type: "json" };
+
+const result = statement(invoices[0], plays);
+
+console.log("\n\n");
+console.log(result);
